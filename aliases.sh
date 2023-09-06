1b627daa91d79b2eaccc1579c4ad5f69d84900c9
Patrick Carton <patrick.d.carton@gmail.com>
2023-09-06 09:56:38 -0400

    add kubectl and replace exa with maintained fork eza

--- aliases.sh
+++ aliases.sh
@@ -2,7 +2,7 @@ alias cat = bat
 alias head = coreutils head
 alias find = fd
 alias du = dust
-alias ls = exa -la --octal-permissions --no-permissions --git --icons
+alias ls = eza -la --octal-permissions --no-permissions --git --icons
 alias branch = git rev-parse --abbrev-ref HEAD
 alias claer = clear
 alias drbash = docker run -it --rm --entrypoint=/bin/bash
